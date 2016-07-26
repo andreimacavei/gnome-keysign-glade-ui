649be783e05b4266f831a2dcce6785ad3ef9c3a4
Andrei Macavei <andrei.macavei89@gmail.com>
2016-07-26 16:11:12 +0300

    Add a scanner widget class.
    This will be used to show camera stream.

--- qrcode.py
+++ qrcode.py
@@ -6,10 +6,11 @@ from gi.repository import Gtk
 log = logging.getLogger(__name__)
 
 
-class QRCodeWidget(Gtk.VBox):
+class QRCodeWidget(Gtk.Box):
 
     def __init__(self, data='Default String'):
         super(QRCodeWidget, self).__init__()
+        self.set_orientation(Gtk.Orientation.VERTICAL)
 
         self.data = data
         self.label = Gtk.Label()
@@ -23,3 +24,15 @@ class QRCodeWidget(Gtk.VBox):
 
     def set_data(self, data):
         self.data = data
+
+
+class QRScannerWidget(Gtk.Box):
+
+    def __init__(self, *args, **kwargs):
+        super(QRScannerWidget, self).__init__()
+        self.set_orientation(Gtk.Orientation.VERTICAL)
+
+        label = Gtk.Label()
+        label.set_markup('<span size="10000">Camera feed</span>')
+
+        self.pack_start(label, True, True, 0)
